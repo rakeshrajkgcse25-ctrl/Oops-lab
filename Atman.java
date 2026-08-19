e23e25043998b15f146fd2a7e5ea75382d6c8fe5
rakeshrajkgcse25-ctrl <rakeshrajkg.cse25@ifet.ac.in>
2026-08-19 11:37:36 +0530

    Add files via upload

---
+++ Atman.java
@@ -0,0 +1,16 @@
+abstract class Animal
+{
+abstract void sound();
+void sleep(){
+system.out println("Anmila is sleeping");
+}}
+class Dog extends Animals
+{
+void sound(){
+system.out println("Dog Barks")
+}}public class main{
+public static void main(string[]arrays){
+Dog d=new dog();
+d.sound();
+d.sleep();}}
+
